3e7f96e885c2bee5ca89b9c6d65574eeedb6023b
Shakthipooja <shakthipoojah@gmail.com>
2026-06-29 15:33:49 +0530

    Update hospital management.py

--- hospital management.py
+++ hospital management.py
@@ -9,17 +9,6 @@ class HospitaL:
 hospital = Hospital("Apollo Hospital")
 #Display
 hospital.display()
-
-# Hospital Class
-class Hospital:
-
-    def __init__(self, hospital_name):
-        self.hospital_name = hospital_name
-
-    def display(self):
-        print("Hospital:", self.hospital_name)
-
-
 # Patient Class
 class Patient:
     def __init__(self, name, age, disease):
@@ -40,5 +29,31 @@ patient = Patient(
 )
 
 # Display details
+hospital.display()
+patient.display()
+# Doctor Class
+class Doctor:
+    def __init__(self, name, specialization):
+        self.name = name
+        self.specialization = specialization
+    def display(self):
+        print("\nDoctor Details")
+        print("Name:", self.name)
+        print("Specialization:", self.specialization)
+# Create objects
+hospital = Hospital("Apollo Hospital")
+patient = Patient(
+    "Arun",
+    45,
+    "Diabetes"
+)
+doctor = Doctor(
+    "Dr. Kumar",
+    "Cardiology"
+)
+# Display
+hospital.display()
+patient.display()
+doctor.display()
 hospital.display()
 patient.display()
