8f0eded5f26b36fe54ffa8021febaa0d4c1c0ac1
Shakthipooja <shakthipoojah@gmail.com>
2026-06-29 15:29:47 +0530

    Add Patient class and display functionality

--- hospital management.py
+++ hospital management.py
@@ -9,4 +9,36 @@ class HospitaL:
 hospital = Hospital("Apollo Hospital")
 #Display
 hospital.display()
-    
+
+# Hospital Class
+class Hospital:
+
+    def __init__(self, hospital_name):
+        self.hospital_name = hospital_name
+
+    def display(self):
+        print("Hospital:", self.hospital_name)
+
+
+# Patient Class
+class Patient:
+    def __init__(self, name, age, disease):
+        self.name = name
+        self.age = age
+        self.disease = disease
+    def display(self):
+        print("\nPatient Details")
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Disease:", self.disease)
+# Create objects
+hospital = Hospital("Apollo Hospital")
+patient = Patient(
+    "Arun",
+    45,
+    "Diabetes"
+)
+
+# Display details
+hospital.display()
+patient.display()
